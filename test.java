9685dfda4f759d61112dba65d02f0c2ac7b273f0
sktm13 <sktm13@naver.com>
2023-05-16 15:47:32 +0900

    123

--- test.java
+++ test.java
@@ -12,6 +12,9 @@ public static void main(String[] args) {
     System.out.println("789");
     System.out.println("789");
     System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
 }
     
 }
