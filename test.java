aeb667a3fe6e35d81cd22309c06c1d3e5fa6835f
sktm13 <sktm13@naver.com>
2023-05-16 15:50:59 +0900

    123

--- test.java
+++ test.java
@@ -8,6 +8,42 @@ public static void main(String[] args) {
     System.out.println("123");
     System.out.println("456");
     System.out.println("789");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("456");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("123");
+    System.out.println("456");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
+    System.out.println("789");
 
 }
     
